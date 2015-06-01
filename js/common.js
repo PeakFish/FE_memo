f28552a2a512b76d620dff71ca07c8e0e3633db2
PeakFish <ro002007@sina.com>
2015-06-01 13:50:55 +0800

    add Function.prototype.bind Polyfill

--- js/common.js
+++ js/common.js
@@ -494,3 +494,37 @@ function getArgs(){
 	}
 	return args; // 返回此对象
 }
+
+
+
+
+/**
+ *  https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Function/bind
+ *  
+ *  Function.prototype.bind Polyfill
+ *
+**/
+
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1), 
+        fToBind = this, 
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP && oThis
+                                 ? this
+                                 : oThis || window,
+                               aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
+  };
+}
