190e92a404126b39a2ad22ac0ed36a5a1570eee6
myName <myEmail>
2017-08-25 10:59:09 +0800

    format

--- js/common.js
+++ js/common.js
@@ -653,12 +653,12 @@ var docCookies = {
 
 //博客园在新标签页中执行代码
 function runCode(id) {
-    obj = document.getElementById(id); 
-    var TestWin=open(''); //打开一个窗口并赋给变量TestWin。 
-    TestWin.opener = null // 防止代码对论谈页面修改 
-    TestWin.document.write(obj.value); //向这个打开的窗口中写入代码code，这样就实现了运行代码功能。 
-    TestWin.document.close(); 
-} 
+    obj = document.getElementById(id);
+    var TestWin=open(''); //打开一个窗口并赋给变量TestWin。
+    TestWin.opener = null // 防止代码对论谈页面修改
+    TestWin.document.write(obj.value); //向这个打开的窗口中写入代码code，这样就实现了运行代码功能。
+    TestWin.document.close();
+}
 
 function LenB(str){
     str = str.replace(/[^\x00-\xff]/g, "**");
